ccf9889bc30df8883c589902431427d3bc1e5b8f
saramohamed124 <saramohamaden1234@gmail.com>
2025-07-08 23:57:30 +0300

    LoginSignUpPages

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+let todos = [];
+
+const inputTextTask = document.getElementById('');
+const btnCreateTask = addEventListener('click', (inputTextTask) => {
+    localStorage.setItem('Task', inputTextTask)
+})
